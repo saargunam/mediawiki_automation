b89bf6ab13f058e7cd02a537ca9a8a1c3e30e43d
saargunam <saargunam17@gmail.com>
2019-01-20 11:36:30 +0530

    terraform formatting and README included

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,8 @@
 output "instance_ids" {
-    value = ["${aws_instance.mw-one.*.public_ip}"]
-    value = ["${aws_instance.mw-two.*.public_ip}"]
+  value = ["${aws_instance.mw-one.*.public_ip}"]
+  value = ["${aws_instance.mw-two.*.public_ip}"]
 }
+
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,16 +1,17 @@
-
 variable "region" {
-  description = "AWS region for hosting our your network"
-  default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
+
 variable "key_name" {
   description = "Key name for SSHing into EC2"
-  default = "deployer-key"
-}
-variable "amis" {
-  description = "Base AMI to launch the instances"
-  default = {
-  us-east-1 = "ami-011b3ccf1bd6db744"
-  }
+  default     = "deployer-key"
 }
 
+variable "amis" {
+  description = "REDHAT BASE AMI "
+
+  default = {
+    us-east-1 = "ami-011b3ccf1bd6db744"
+  }
+}
